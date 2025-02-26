e933de60421dae8ecab9fcf63b3fcffd6d3f95d4
NikhilProgrammer01 <nikhilgohil316@gmail.com>
2025-02-26 11:41:04 +0530

    added changes

--- models/songModel.js
+++ models/songModel.js
@@ -2,11 +2,12 @@ const pool = require("../config/db");
 
 const getSongsByName = async (name) => {
   const query = `
-    SELECT * FROM songs 
-    WHERE title ILIKE $1
-    ORDER BY title ASC
-  `;
-  const result = await pool.query(query, [`${name}%`]); // Match from start
+  SELECT * FROM songs 
+  WHERE title ILIKE '%' || $1 || '%'
+  ORDER BY title ASC
+`;
+const result = await pool.query(query, [name]); // Remove `%`
+
   return result.rows;
 };
 
