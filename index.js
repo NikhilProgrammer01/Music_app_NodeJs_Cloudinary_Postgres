fd5810202a18eb52f30bec8a23317c7d3957b9c2
NikhilProgrammer01 <nikhilgohil316@gmail.com>
2025-02-27 13:25:41 +0530

    vhnaged version 18

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 require("dotenv").config();
-const dbRoutes = require("./routes/dbRoutes");
+// const dbRoutes = require("./routes/dbRoutes");
 const songRoutes = require("./routes/songRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use("/api", songRoutes);
-app.use("/api", dbRoutes);
+// app.use("/api", dbRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
--- routes/dbRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { checkDbConnection } = require("../config/db");
-
-const router = express.Router();
-
-// Endpoint to check database connection
-router.get("/check-db", checkDbConnection);
-
-module.exports = router;
